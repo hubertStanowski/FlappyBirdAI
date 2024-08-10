d23f322f8c5c3fa81432f0b55dda9276a08224dc
Hubert Stanowski <hubert.stanowski@gmail.com>
2024-08-10 17:53:40 +0200

    Add background

--- constants.py
+++ constants.py
@@ -8,6 +8,7 @@ GRAVITY = 0.5
 
 PLAYER_IMG = pygame.image.load("images/bird.png")
 GROUND_IMG = pygame.image.load("images/ground.png")
+BACKGROUND_IMG = pygame.image.load("images/background.png")
 
 WHITE = (255, 255, 255)
 SKY_COLOR = (102, 178, 255)
--- main.py
+++ main.py
@@ -11,16 +11,16 @@ def main():
     pygame.display.set_caption("Flappy Bird AI")
 
     clock = pygame.time.Clock()
-    player = Player(100, 100)
+    player = Player(WINDOW_WIDTH // 3, WINDOW_HEIGHT // 2)
     ground_scroll = 0
+    started = False
 
     while True:
         clock.tick(60)
-
-        window.fill(SKY_COLOR)
-        player.draw(window)
         window.blit(GROUND_IMG, (ground_scroll,
                     WINDOW_HEIGHT-GROUND_IMG.get_height()))
+        window.blit(BACKGROUND_IMG, (0, 0))
+        player.draw(window)
 
         ground_scroll -= SCROLL_SPEED
 
@@ -28,13 +28,15 @@ def main():
             ground_scroll = 0
 
         pygame.display.update()
-        player.move()
+        if started:
+            player.move()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
+                    started = True
                     player.flap()
 
 
