245d91be34b14e21f2b6d1bb8777a0539b36e0d8
Hubert Stanowski <hubert.stanowski@gmail.com>
2024-08-25 19:20:29 +0200

    Add a config file for others to modify with no risk of accidental changes

---
+++ source/user_config.py
@@ -0,0 +1,11 @@
+# play around with POPULATION_SIZE if you want but to see evolution best keep it between 50-150
+POPULATION_SIZE = 100
+
+# lower DRAW_LIMIT if badly lagging
+DRAW_LIMIT = 200
+
+# if you are impatient lower POPULATION_STALENESS_LIMIT, but recommended above 5
+POPULATION_STALENESS_LIMIT = 10
+
+# if you want to play the game yourself set HUMAN_PLAYING to True
+HUMAN_PLAYING = False
